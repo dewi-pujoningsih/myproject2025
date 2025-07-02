36a3cbac470fe09f2af14d275249597f270213aa
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-02 16:10:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
-
-st.title("🎈 My Calculator 2025")
+st.title(":blue[Kalkulator Aritmatika]:rocket:")
+st.header(":red[Aplikasi untuk menghitung nilai faktorial]")
+number1 = st.number_input("masukkan ")
+number2 = st.number_input("masukkan angka 2")
+if st.checkbox("hasil kali"):
+    st.write(number1*number2)
+if st.checkbox("hasil bagi"):
+    st.write(number1/number2)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
