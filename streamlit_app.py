88351cb4f02f4cbbcfd4933c82975851fa68f78a
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-07-04 20:18:04 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,2 @@
 import streamlit as st
 st.title("My Project 2025")
-
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
